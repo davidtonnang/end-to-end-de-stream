bbb08bd415e1aa13561dcd731915a1abc0e386b5
David Tönnäng <davidtonnang@outlook.com>
2024-03-29 21:45:56 +0100

    fixed udf definitions

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from pyspark.sql import SparkSession
+from pyrspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
 from config.config import configuration
+from udf_utils import *
+
+def define_udfs():
+    return {
+        'extract_file_name_udf': udf(extract_file_name, StringType()),
+        'extract_position_udf': udf(extract_position, StringType()),
+        'extract_salary_udf': udf(extract_salary, StructType([
+            StructField('salary_start', DoubleType(), True),
+            StructField('salary_end', DoubleType(), True)
+        ])),
+        'extract_date_udf': udf(extract_start_date, DateType()),
+        'extract_enddate_udf': udf(extract_end_date, DateType()),
+        'extract_classcode_udf': udf(extract_class_code, StringType()),
+        'extract_requirements_udf': udf(extract_requirements, StringType()),
+        'extract_notes_udf': udf(extract_notes, StringType()),
+        'extract_duties_udf': udf(extract_duties, StringType()),
+        'extract_selection_udf': udf(extract_selection, StringType()),
+        'extract_experience_length_udf': udf(extract_experience_length, StringType()),
+        'extract_education_length_udf': udf(extract_education_length, StringType()),
+        'extract_application_location_udf': udf(extract_application_location, StringType()),
+    }
 
 if __name__ == "__main__":
     spark = (SparkSession.builder.appName('AWS_Spark_Unstructured')
